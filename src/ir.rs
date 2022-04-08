adcdd2fca2e19da2f22cbc06df2cb91d068dae05
kgtkr <contact@kgtkr.net>
2022-04-08 22:59:55 +0900

    ir変更

--- src/ir.rs
+++ src/ir.rs
@@ -4,10 +4,29 @@ pub struct Module {
 }
 
 #[derive(Debug, PartialEq, Clone)]
+pub struct IfInfo {
+    pub if_: usize,
+    pub else_: usize,
+    pub if_end: usize,
+}
+
+#[derive(Debug, PartialEq, Clone)]
+pub struct LoopInfo {
+    pub loop_: usize,
+    pub loop_then: usize,
+    pub loop_end: usize,
+}
+
+#[derive(Debug, PartialEq, Clone)]
 pub struct Func {
     pub args_count: usize,
     pub instrs: Vec<Instr>,
+    pub if_infos: Vec<IfInfo>,
+    pub loop_infos: Vec<LoopInfo>,
 }
+
+pub type LoopId = usize;
+pub type IfId = usize;
 
 #[derive(Debug, PartialEq, Clone)]
 pub enum Instr {
@@ -17,14 +36,13 @@ pub enum Instr {
     PrefixOp(PrefixOp),
     Assign(usize),
     Call(usize),
-    While(usize, usize),
-    If(usize),   // Else
-    Else(usize), // EndIf
-    EndIf,
-    Loop,
-    LoopThen(usize), // EndLoop
-    EndLoop(usize),  // Loop
-    VarDef(usize, usize),
+    If(IfId),
+    Else(IfId),
+    EndIf(IfId),
+    Loop(LoopId),
+    LoopThen(LoopId),
+    EndLoop(LoopId),
+    VarDef,
 }
 
 #[derive(Debug, PartialEq, Clone)]
