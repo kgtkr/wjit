986763467057d02061eaed04c811337dd63eef65
kgtkr <contact@kgtkr.net>
2022-04-10 20:20:57 +0900

    runnerのコードをきれいに

--- runner.js
+++ runner.js
@@ -1,16 +1,20 @@
-const fs = require("fs/promises");
+"use strict";
+const fs = require("fs");
 
-const wasmPath = "target/wasm32-unknown-unknown/debug/wjit.wasm";
-const dumpWasm = process.argv.includes("--dump-wasm");
+class Runner {
+  constructor() {
+    this.dumpWasm = process.argv.includes("--dump-wasm");
 
-(async () => {
-  const wasmBin = await fs.readFile(wasmPath);
-  const wasmInstance = (await WebAssembly.instantiate(wasmBin, {})).instance;
+    const wasmPath = "target/wasm32-unknown-unknown/debug/wjit.wasm";
+    const wasmBin = fs.readFileSync(wasmPath);
+    const wasmModule = new WebAssembly.Module(wasmBin);
+    this.wasmInstance = new WebAssembly.Instance(wasmModule);
+  }
 
-  function stringToPtr(str) {
+  stringToPtr(str) {
     const buf = Buffer.from(str);
-    const ptr = wasmInstance.exports.alloc(buf.length + 1);
-    const memoryView = new DataView(wasmInstance.exports.memory.buffer);
+    const ptr = this.wasmInstance.exports.alloc(buf.length + 1);
+    const memoryView = new DataView(this.wasmInstance.exports.memory.buffer);
 
     for (let i = 0; i < buf.length; i++) {
       memoryView.setUint8(ptr + i, buf[i]);
@@ -19,54 +23,57 @@ const dumpWasm = process.argv.includes("--dump-wasm");
     return ptr;
   }
 
-  function ptrToBuffer(ptr, len) {
-    const buf = wasmInstance.exports.memory.buffer;
+  ptrToBuffer(ptr, len) {
+    const buf = this.wasmInstance.exports.memory.buffer;
     return buf.slice(ptr, ptr + len);
   }
 
-  const code = await fs.readFile(process.argv[2], { encoding: "utf8" });
-
-  const irModulePtr = wasmInstance.exports.make_ir_module(stringToPtr(code));
-  const compilerPtr = wasmInstance.exports.make_compiler(irModulePtr);
-
-  const skeletonBinLenPtr = wasmInstance.exports.alloc(4);
-  const skeletonBinPtr = wasmInstance.exports.compile_skeleton(
-    compilerPtr,
-    skeletonBinLenPtr
-  );
-
-  const memoryView = new DataView(wasmInstance.exports.memory.buffer);
-  const skeletonBinLen = memoryView.getInt32(skeletonBinLenPtr, true);
-  const skeletonBin = ptrToBuffer(skeletonBinPtr, skeletonBinLen);
-  if (dumpWasm) {
-    await fs.writeFile(`dump_wasm/skeleton.wasm`, Buffer.from(skeletonBin));
+  makeIrModule(code) {
+    return this.wasmInstance.exports.make_ir_module(this.stringToPtr(code));
   }
 
-  let table;
-  const skeletonInstance = (
-    await WebAssembly.instantiate(skeletonBin, {
+  makeCompiler(irModule) {
+    return this.wasmInstance.exports.make_compiler(irModule);
+  }
+
+  makeSkeletonModule(compiler) {
+    const skeletonBinLenPtr = this.wasmInstance.exports.alloc(4);
+    const skeletonBinPtr = this.wasmInstance.exports.compile_skeleton(
+      compiler,
+      skeletonBinLenPtr
+    );
+    const memoryView = new DataView(this.wasmInstance.exports.memory.buffer);
+    const skeletonBinLen = memoryView.getInt32(skeletonBinLenPtr, true);
+    const skeletonBin = this.ptrToBuffer(skeletonBinPtr, skeletonBinLen);
+    if (this.dumpWasm) {
+      fs.writeFileSync(`dump_wasm/skeleton.wasm`, Buffer.from(skeletonBin));
+    }
+    return new WebAssembly.Module(skeletonBin);
+  }
+
+  makeSkeltonInstance(compiler, skeletonModule) {
+    const skeletonInstance = new WebAssembly.Instance(skeletonModule, {
       env: {
         compile_func: (idx) => {
           console.log("compile_func", idx);
-          const funcBinLenPtr = wasmInstance.exports.alloc(4);
-          const funcBinPtr = wasmInstance.exports.compile_func(
-            compilerPtr,
+          const funcBinLenPtr = this.wasmInstance.exports.alloc(4);
+          const funcBinPtr = this.wasmInstance.exports.compile_func(
+            compiler,
             idx,
             funcBinLenPtr
           );
-          const memoryView = new DataView(wasmInstance.exports.memory.buffer);
+          const memoryView = new DataView(
+            this.wasmInstance.exports.memory.buffer
+          );
           const funcBinLen = memoryView.getInt32(funcBinLenPtr, true);
-          const funcBin = ptrToBuffer(funcBinPtr, funcBinLen);
-          if (dumpWasm) {
-            require("fs").writeFileSync(
-              `dump_wasm/${idx}.wasm`,
-              Buffer.from(funcBin)
-            );
+          const funcBin = this.ptrToBuffer(funcBinPtr, funcBinLen);
+          if (this.dumpWasm) {
+            fs.writeFileSync(`dump_wasm/${idx}.wasm`, Buffer.from(funcBin));
           }
           const funcModule = new WebAssembly.Module(funcBin);
           new WebAssembly.Instance(funcModule, {
             env: {
-              _table: table,
+              _table: skeletonInstance.exports._table,
               println: (x) => {
                 console.log(x);
                 return 0;
@@ -76,9 +83,16 @@ const dumpWasm = process.argv.includes("--dump-wasm");
           return 0;
         },
       },
-    })
-  ).instance;
-  table = skeletonInstance.exports._table;
+    });
+    return skeletonInstance;
+  }
+}
 
-  skeletonInstance.exports.main();
-})();
+const code = fs.readFileSync(process.argv[2], { encoding: "utf8" });
+const runner = new Runner();
+const irModule = runner.makeIrModule(code);
+const compiler = runner.makeCompiler(irModule);
+const skeletonModule = runner.makeSkeletonModule(compiler);
+const skeletonInstance = runner.makeSkeltonInstance(compiler, skeletonModule);
+
+skeletonInstance.exports.main();
